fff3b0404b376e6fa8bee20add6b6935cbf5dcd3
mecarm <melecarmine99@gmail.com>
2023-01-19 15:12:24 +0100

    Imposto cartelle e route iniziali, header completo

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg w-75 m-auto">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{route('home')}}">
+            <img src="{{ asset('img/dc-logo.png') }}" alt="DC logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{route('home')}}">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">CHARACTERS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">COMICS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">MOVIES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">TV</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">GAMES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">COLLECTIBLES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">VIDEOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">FANS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">NEWS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">SHOP</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
